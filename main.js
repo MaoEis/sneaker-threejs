a8032465e1c7c3a22ece3e8527e746b981663f81
Mai Eisenberg <r0377155@student.thomasmore.be>
2024-11-25 14:50:59 +0100

    fix: select color to object

--- main.js
+++ main.js
@@ -30,9 +30,6 @@ controls.enableRotate = true;
 // Limit y-axis (vertical) movement
 controls.maxPolarAngle = Math.PI / 2; // Adjust this value as needed
 controls.minPolarAngle = Math.PI / 4; // Adjust this value as needed
-// Disable x-axis (horizontal) movement
-// controls.maxAzimuthAngle = 0; // Lock horizontal rotation
-// controls.minAzimuthAngle = 0; // Lock horizontal rotation
 //limit z-axis movement
 controls.minDistance = 6; // Adjust this value as needed
 controls.maxDistance = 10; // Adjust this value as needed
@@ -70,47 +67,62 @@ scene.add(backgroundSphere);
 // add shoe.glb
 let shoe;
 const shoeMeshes = [];
-let isDragging = false;
-
-document.addEventListener("mousedown", (event) => {
-  isDragging = true;
-  previousMouseX = event.clientX;
-});
-
-document.addEventListener("mouseup", () => {
-  isDragging = false;
-});
 
 const loader = new GLTFLoader();
 loader.load(
   "shoe.glb",
   (gltf) => {
-    // change material to standard white(.map)
-    gltf.scene.traverse((child) => {
-      if (child.isMesh) {
-        child.material = new THREE.MeshStandardMaterial({
-          color: 0xffffff,
-          metalness: 0.2,
-          roughness: 0.5,
-        });
-      }
-    });
     shoe = gltf.scene;
     shoe.position.y = 2;
     shoe.scale.set(1, 1, 1);
     shoe.castShadow = true;
     shoe.receiveShadow = true;
-    scene.add(shoe);
     shoe.rotation.y = Math.PI / -2;
 
-    // add on click shoe movement turns around
-    // let previousMouseX = 0;
-    // document.addEventListener("mousemove", (event) => {
-    //   if (isDragging) {
-    //     const movementX = event.movementX || 0;
-    //     shoe.rotation.y += (movementX * Math.PI) / 180 / 4;
-    //   }
-    // });
+    const leatherTexture = new THREE.TextureLoader().load(
+      "/fabrics/leather.jpg"
+    );
+    const leatherNormal = new THREE.TextureLoader().load(
+      "/fabrics/leatherNorm.jpg"
+    );
+    const leatherReflect = new THREE.TextureLoader().load(
+      "/fabrics/leatherReflect.jpg"
+    );
+    const leatherGloss = new THREE.TextureLoader().load(
+      "/fabrics/leatherGloss.jpg"
+    );
+    leatherTexture.wrapS = THREE.RepeatWrapping;
+    leatherTexture.wrapT = THREE.RepeatWrapping;
+    leatherTexture.repeat.set(3, 3);
+
+    const shoeMaterial = new THREE.MeshStandardMaterial({
+      color: 0xffffff,
+      normalMap: leatherNormal,
+      displacementMap: leatherTexture,
+      displacementScale: 0.1,
+      envMap: leatherReflect,
+      roughnessMap: leatherGloss,
+    });
+
+    scene.add(shoe);
+
+    const lacesMesh = shoe.getObjectByName("laces");
+    const soleBottomMesh = shoe.getObjectByName("sole_bottom");
+    const soleTopMesh = shoe.getObjectByName("sole_top");
+    const insideMesh = shoe.getObjectByName("inside");
+    const outside1Mesh = shoe.getObjectByName("outside_1");
+    const outside2Mesh = shoe.getObjectByName("outside_2");
+    const outside3Mesh = shoe.getObjectByName("outside_3");
+
+    shoeMeshes.push(
+      lacesMesh,
+      soleBottomMesh,
+      soleTopMesh,
+      insideMesh,
+      outside1Mesh,
+      outside2Mesh,
+      outside3Mesh
+    );
   },
 
   undefined,
@@ -119,80 +131,10 @@ loader.load(
   }
 );
 
-//change color function
-function onColorOptionClick(event) {
-  const selectedColor = new THREE.Color(
-    parseInt(event.target.dataset.color, 16)
-  );
-  // Apply the selected color to the entire shoe
-  if (selectedPart) {
-    switch (selectedPart.name) {
-      case "Laces":
-        customizationData.lacesColor = selectedColor;
-        break;
-      case "Sole bottom":
-        customizationData.soleBottomColor = selectedColor;
-        break;
-      case "sole top":
-        customizationData.soleTopColor = selectedColor;
-        break;
-      case "Lining":
-        customizationData.liningColor = selectedColor;
-        break;
-      case "Front part":
-        customizationData.frontPartColor = selectedColor;
-        break;
-      case "Upper part":
-        customizationData.upperPartColor = selectedColor;
-        break;
-      case "Body":
-        customizationData.bodyColor = selectedColor;
-        break;
-
-      // Add cases for other parts as needed
-    }
-    if (selectedPart.material) {
-      selectedPart.material.color.copy(selectedColor);
-      partColors.set(selectedPart.uuid, selectedColor);
-    }
-
-    // Add or remove the 'selected' class based on the selected color
-    const selectedBox = document.querySelector(".box.selected");
-    if (selectedBox) {
-      selectedBox.classList.remove("selected");
-    }
-    event.target.classList.add("selected");
-  }
-}
+// move event select object in shoe when hoover
 //make a raycaster
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
-
-//mouse click
-// window.addEventListener("click", (event) => {
-//   pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-//   raycaster.setFromCamera(pointer, camera);
-
-//   const intersects = raycaster.intersectObjects(scene.children, true);
-
-//   if (intersects.length > 0) {
-//     const firstIntersect = intersects[0];
-//     if (
-//       firstIntersect.object.material &&
-//       firstIntersect.object.material.color
-//     ) {
-//       firstIntersect.object.material.color.set(0xff0000);
-//     }
-//     console.log(
-//       "Clicked object:",
-//       firstIntersect.object.name || firstIntersect.object
-//     );
-//   }
-// });
-// add a mouse click event with a gsap animation to move the camera to the clicked object for every object
-
 // Store original colors of the objects
 scene.children.forEach((child) => {
   if (child.material && child.material.color) {
@@ -201,7 +143,6 @@ scene.children.forEach((child) => {
 });
 
 let lastHoveredObject = null;
-
 window.addEventListener("mousemove", (event) => {
   pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
   pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
@@ -218,7 +159,9 @@ window.addEventListener("mousemove", (event) => {
         intersects.length === 0 ||
         !intersects.some((intersect) => intersect.object === child)
       ) {
-        child.material.color.set(child.originalColor || 0xffffff);
+        if (!child.userData.selectedColor) {
+          child.material.color.set(child.originalColor || 0xffffff);
+        }
       }
     }
   });
@@ -243,9 +186,11 @@ window.addEventListener("mousemove", (event) => {
         intersectedObject.material.emissive.set(0xc702fe); // Highlight in red
       }
       if (lastHoveredObject && lastHoveredObject !== intersectedObject) {
-        lastHoveredObject.material.color.set(
-          lastHoveredObject.originalColor || 0xffffff
-        );
+        if (!lastHoveredObject.userData.selectedColor) {
+          lastHoveredObject.material.color.set(
+            lastHoveredObject.originalColor || 0xffffff
+          );
+        }
         lastHoveredObject.material.emissive.set(0x000000);
       }
 
@@ -254,9 +199,11 @@ window.addEventListener("mousemove", (event) => {
     } else {
       // When no object is intersected, reset the last hovered object if any
       if (lastHoveredObject) {
-        lastHoveredObject.material.color.set(
-          lastHoveredObject.originalColor || 0xffffff
-        );
+        if (!lastHoveredObject.userData.selectedColor) {
+          lastHoveredObject.material.color.set(
+            lastHoveredObject.originalColor || 0xffffff
+          );
+        }
         lastHoveredObject.material.emissive.set(0x000000);
         lastHoveredObject = null; // Clear the reference
       }
@@ -264,30 +211,7 @@ window.addEventListener("mousemove", (event) => {
   }
 });
 
-//   pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-//   raycaster.setFromCamera(pointer, camera);
-
-//   const intersects = raycaster.intersectObjects(scene.children, true);
-
-//   if (intersects.length > 0) {
-//     const firstIntersect = intersects[0];
-//     if (
-//       firstIntersect.object.material &&
-//       firstIntersect.object.material.color
-//     ) {
-//       firstIntersect.object.material.color.set(0x00ff00); // Highlight in green
-//     }
-//     gsap.to(camera.position, {
-//       x: firstIntersect.point.x,
-//       y: firstIntersect.point.y,
-//       z: firstIntersect.point.z,
-//       duration: 1,
-//     });
-//   }
-// });
-
+// click event select object in shoe and camera animation
 function styleNavChildren() {
   const nav = document.querySelector(".nav");
   const children = nav.children;
@@ -296,7 +220,6 @@ function styleNavChildren() {
     children[i].style.fontWeight = "normal";
   }
 }
-
 function animateCamera(position, targetClass) {
   gsap.to(camera.position, {
     x: position.x,
@@ -311,6 +234,20 @@ function animateCamera(position, targetClass) {
   document.querySelector(targetClass).style.opacity = "1";
   document.querySelector(targetClass).style.fontWeight = "bold";
 }
+
+let selectedColor = null;
+document.querySelectorAll(".colorOption .box").forEach((box) => {
+  box.addEventListener("click", (event) => {
+    selectedColor = `#${event.target.getAttribute("data-color")}`;
+    // Apply the selected color to the last selected object
+    if (lastSelectedObject && selectedColor) {
+      lastSelectedObject.material.color.set(selectedColor);
+      lastSelectedObject.userData.selectedColor = selectedColor; // Store the selected color
+    }
+  });
+});
+
+let lastSelectedObject = null;
 
 window.addEventListener("click", (event) => {
   // Get the mouse coordinates in normalized device coordinates
@@ -327,35 +264,48 @@ window.addEventListener("click", (event) => {
   if (intersects.length > 0) {
     const firstIntersect = intersects[0];
     const intersectedObject = firstIntersect.object;
+    const shoeParts = [
+      "laces",
+      "sole_bottom",
+      "sole_top",
+      "inside",
+      "outside_1",
+      "outside_2",
+      "outside_3",
+    ];
 
-    // Check if the intersected object has a name and animate the camera accordingly
-    switch (intersectedObject.name) {
-      case "laces":
-        animateCamera({ x: -9, y: 9, z: 2 }, ".laces");
-        break;
-      case "outside_1":
-        animateCamera({ x: -2, y: 6, z: 4 }, ".outside_1");
-        break;
-      case "outside_2":
-        animateCamera({ x: 3, y: 6, z: 4 }, ".outside_2");
-        break;
-      case "outside_3":
-        animateCamera({ x: -6, y: 5.5, z: 3 }, ".outside_3");
-        break;
-      case "inside":
-        animateCamera({ x: -6, y: 7, z: 3 }, ".inside");
-        break;
-      case "sole_bottom":
-        animateCamera({ x: -2, y: 0, z: 6 }, ".sole_bottom");
-        break;
-      case "sole_top":
-        animateCamera({ x: -6, y: 5, z: 6 }, ".sole_top");
-        break;
+    if (shoeParts.includes(intersectedObject.name)) {
+      // Set the last selected object
+      lastSelectedObject = intersectedObject;
+
+      // Check if the intersected object has a name and animate the camera accordingly
+      switch (intersectedObject.name) {
+        case "laces":
+          animateCamera({ x: -9, y: 9, z: 2 }, ".laces");
+          break;
+        case "outside_1":
+          animateCamera({ x: -2, y: 6, z: 4 }, ".outside_1");
+          break;
+        case "outside_2":
+          animateCamera({ x: 3, y: 6, z: 4 }, ".outside_2");
+          break;
+        case "outside_3":
+          animateCamera({ x: -6, y: 5.5, z: 3 }, ".outside_3");
+          break;
+        case "inside":
+          animateCamera({ x: -6, y: 7, z: 3 }, ".inside");
+          break;
+        case "sole_bottom":
+          animateCamera({ x: -2, y: 0, z: 6 }, ".sole_bottom");
+          break;
+        case "sole_top":
+          animateCamera({ x: -6, y: 5, z: 6 }, ".sole_top");
+          break;
+      }
     }
   }
 });
 
-// Add event listeners to the navigation buttons
 document.getElementById("laces").addEventListener("click", () => {
   animateCamera({ x: -9, y: 9, z: 2 }, ".laces");
 });
@@ -378,45 +328,6 @@ document.getElementById("sole_top").addEventListener("click", () => {
   animateCamera({ x: -6, y: 5, z: 6 }, ".sole_top");
 });
 
-const gui = new GUI();
-const settings = {
-  lightIntensity: 0,
-  shoeX: 0,
-  shoeY: 0,
-  shoeZ: 0,
-  rotationSpeed: 0,
-};
-
-gui
-  .add(settings, "lightIntensity", -2, 2, 0.1)
-  .name("Light Intensity")
-  .onChange((value) => {
-    directionalLight.intensity = value;
-  });
-
-gui
-  .add(settings, "shoeX", -5, 5, 0.1)
-  .name("Shoe X Position")
-  .onChange((value) => {
-    shoe.position.x = value;
-  });
-
-gui
-  .add(settings, "shoeY", -5, 5, 0.1)
-  .name("Shoe Y Position")
-  .onChange((value) => {
-    shoe.position.y = value;
-  });
-
-gui
-  .add(settings, "shoeZ", -5, 5, 0.1)
-  .name("Shoe Z Position")
-  .onChange((value) => {
-    shoe.position.z = value;
-  });
-
-gui.add(settings, "rotationSpeed", 0, 0.1, 0.01).name("Rotation Speed");
-
 // Position the camera
 camera.position.z = 10;
 camera.position.y = 3;
@@ -425,11 +336,11 @@ camera.position.y = 3;
 function animate() {
   requestAnimationFrame(animate);
 
-  // shoe.rotation.y += settings.rotationSpeed;
-
   controls.update(); // Required if damping is enabled
   renderer.render(scene, camera);
   raycaster.setFromCamera(pointer, camera);
+  //set camera to look at the shoe
+  camera.lookAt(shoe.position);
 }
 
 // Handle window resizing
